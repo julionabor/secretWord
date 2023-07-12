bd9cc3be194ce42f84edb016ce9b0afefa685b4a
Julio Nabor <julionabor@gmail.com>
2023-07-12 10:54:45 +0100

    Project secret Word using differents hooks

---
+++ src/data/words.js
@@ -0,0 +1,7 @@
+export const wordsList = {
+    react: ["State", "hooks", "Redux", "Context", "useEffect"],
+    css: ["float", "margin", "padding", "flexbox", "display"],
+    html5: ["nav", "main", "aside", "header", "footer"],
+    javascript: ["includes", "map", "filter", "innerHtml"],
+    git: ["push", "commit", "merge", "pull"],
+  };
